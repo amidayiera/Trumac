c285bad5e87262515ef221905590f760ef20e7cb
Kevin Macharia <kevinjmacharia@gmail.com>
2019-09-25 09:29:01 +0300

    Add files via upload

---
+++ 2019_09_24_202853_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->integer('product id');
+            $table->integer('user id');
+            $table->string('product name');
+            $table->integer('product price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
