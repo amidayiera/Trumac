f765b633e32a049b26f9b36f98c3de2580fb8506
Nasha <amidayiera@gmail.com>
2019-10-14 00:23:53 +0300

    updated image display

--- resources/views/prodMaster.blade.php
+++ resources/views/prodMaster.blade.php
@@ -4,24 +4,27 @@
    @foreach($details as $detail)
        <div class="row container mx-auto mt-lg-5">
            <div class="col-md-3">
-               <img src="{{asset($detail->image)}}" width="450px" height="450px">
+               <img src="{{asset('storage/images/' .$detail->image)}}" width="450px" height="500px">
            </div>
            <div style="text-align:center; padding-left: 250px;">
                    <br><br><br><br>
            <h1 text-align="center">{{$detail->name }}</H1>
+
+            <br>
+             <h3 text-align="center">Purchase Price : Ksh{{ $detail->purchase_price }}</h3>
+             <h4>Refill Price : Ksh {{$detail->refill_price}}</h4>
              <br><br><br>
              <button type="button" class="btn btn-outline-danger">Add To Cart</button>
              <button type="button" class="btn btn-outline-success" >Buy Now</button>
-             
-             
+             <br><br>
            </div>
        </div>
-       
+{{--        
            <div class=" container" style="background-color: #e3f2fd; padding:20px; margin-top:25px; text-align:center;">
                <h1 style="text-transform: uppercase; text-decoration:underline; font-size:24px;"> Description</h1>
                <h4>Purchase price : {{$detail->purchase_price}} ksh</h4>
                <h4>Refill price :{{ $detail->refill_price }} ksh</h4>
            
-       </div>
+       </div> --}}
     @endforeach
 @endsection
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -7,20 +7,18 @@
       <div text-align="center">
           <div class="row"> 
             <div class="col-sm-3">
-            <a href="{{ url('prodMaster', $bott->id) }}"><img class="img-thumbnail"src = "{{ asset($bott->image) }}" width="250px" height="400px"></a> 
+            <a href="{{ url('prodMaster', $bott->id) }}"><img class="img-thumbnail"src = "{{ asset('storage/images/' .$bott->image) }}" width="250px" height="400px"></a> 
             </div>  
             <div class="col-sm-9">
               <br>
               <div style="padding-left:60px">
-              <h1 text-align="center">{{ $bott->name }}</H1>
-                <br>
+              <h1 text-align="center" color="#00078a">{{ $bott->name }}</H1>
+                <h3 text-align="center">Purchase Price : Ksh{{ $bott->purchase_price }}</h3>
+                <h4>Refill Price : Ksh {{$bott->refill_price}}</h4>
                 <!--ADD KEVINS ADD TO CART LINK HERE--><button type="button" class="btn btn-outline-danger">Add To Cart</button>
                 <a href="{{ url('prodMaster', $bott->id) }}"><button type="button" class="btn btn-outline-Success" >Buy  Now</button></a>
                 <button class="btn btn-outline-info">Request Refill</button>
-                <br><br>
-                <h3 text-align="center">Purchase Price : Ksh{{ $bott->purchase_price }}</h3>
-              <h4>Refill Price : Ksh {{$bott->refill_price}}</h4>
-            </div>
+              </div>
           </div>
           </div>
       </div>
